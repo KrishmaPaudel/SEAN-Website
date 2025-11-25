847fc46159468fb840f1a3cc292e48ba81e1c8c7
Rajan Raj Acharya <acharya.rajan.raj11@gmail.com>
2025-11-25 20:28:01 +0545

    refactor(blog): make different for creating, and fetching blogs, -updated controller to fetch total count of blogs in single request and updated response

--- backend/src/routes/blog.routes.js
+++ backend/src/routes/blog.routes.js
@@ -3,6 +3,7 @@ import { createBlog, getAllBlogs } from "../controllers/blog.controllers.js";
 
 const router = Router();
 
-router.route("/").post(createBlog).get(getAllBlogs);
+router.route("/").post(createBlog);
+router.route("/filter").get(getAllBlogs);
 
 export default router;
