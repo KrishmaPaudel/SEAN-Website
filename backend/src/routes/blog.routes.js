103aac46275c7e37607cd7d0bd824ad7087c027b
Samyak <samyakraj6@gmail.com>
2025-11-29 22:59:21 +0545

    feat(auth): add authentication middleware for blog routes

--- backend/src/routes/blog.routes.js
+++ backend/src/routes/blog.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { createBlog, getAllBlogs, updateBlog, deleteBlog } from "../controllers/blog.controllers.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
+
 const router = Router();
 
 router.route("/").post(createBlog);
 router.route("/filter").get(getAllBlogs);
-router.route("/:id").put(authMiddleware, updateBlog).delete(authMiddleware, deleteBlog);
+router.route("/update/:id").put(authMiddleware, updateBlog);
+router.route("/delete/:id").delete(authMiddleware, deleteBlog);
 
 export default router;
